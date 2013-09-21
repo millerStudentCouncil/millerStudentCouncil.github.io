9e9bc984ab52555f54c575ec60888a5d61494684
president <president>
2013-09-21 13:11:24 -0700

    YAAAAAAAY All main page data stored in JSON

--- hashSwitching.js
+++ hashSwitching.js
@@ -1,5 +1,83 @@
 var defaultFragment = "main"
 
+function createUpcomingEvent(data) {
+	var event = document.createElement('div')
+	event.className = 'item'
+
+	var img = document.createElement('img')
+	img.style.height = '300px'
+	img.style.display = 'inline-block'
+	img.setAttribute('src', data.img)
+	event.appendChild(img)
+
+	var i = 0
+	for (i = 0; i<2; i++) {
+		var spacer = document.createElement('h1')
+		spacer.innerHTML = "&nbsp;"
+		event.appendChild(spacer)
+	}
+
+	var caption = document.createElement('div')
+	caption.className = 'carousel-caption'
+
+	var title = createTitle(data.title)
+	caption.appendChild(title)
+
+	if ((typeof data.info) === 'string') {
+		var info = document.createElement('p')
+		info.innerHTML = data.info
+		caption.appendChild(info)
+	}
+
+	event.appendChild(caption)
+
+	return event
+}
+
+function createAnnouncement(data) {
+	var announcement = document.createElement('li')
+	var title = createTitle(data.title)
+	announcement.appendChild(title)
+	if ((typeof data.eventInfo === 'object')) {
+		var currentInfo = 0
+		for (currentInfo = 0; currentInfo < data.eventInfo.length; currentInfo++) {
+			var infoData = data.eventInfo[currentInfo]
+			if ((typeof infoData) === 'object') {
+				var infoTitle = document.createElement('h4')
+				infoTitle.innerHTML = infoData.title
+
+				var infoText = document.createElement('p')
+				infoText.innerHTML = infoData.text
+
+				announcement.appendChild(infoTitle)
+				announcement.appendChild(infoText)
+			} else if ((typeof infoData) === 'string') {
+				var infoTitle = document.createElement('h4')
+				infoTitle.innerHTML = infoData
+
+				announcement.appendChild(infoTitle)
+			}
+		}
+	}
+	return announcement
+}
+
+function createTitle(data) {
+	var title = document.createElement('h3')
+	if ((typeof data) === 'string') {
+		title.innerHTML = data
+	} else if ((typeof data) === 'object') {
+		if ((typeof data.link) === 'string') {
+			var link = document.createElement('a')
+			link.setAttribute('href', data.link)
+			link.innerHTML = data.text
+			title.appendChild(link)
+		}
+	}
+
+	return title
+}
+
 function replaceFragment(hash) {
 	if (hash instanceof String && hash.startsWith("#")) {
 		hash = hash.substring(1)
@@ -19,6 +97,24 @@ function replaceFragment(hash) {
 			$("#upcoming-events").carousel({
 				interval: 3000
 			})
+
+			var currentAnnouncement = 0
+			for (currentAnnouncement = 0; currentAnnouncement < announcements.length; currentAnnouncement++) {
+				var announcementsList = document.getElementById('announcements')
+				var announcement = createAnnouncement(announcements[currentAnnouncement])
+				announcementsList.appendChild(announcement)
+			}
+
+			var currentEvent = 0
+			for (currentEvent = 0; currentEvent < upcomingEvents.length; currentEvent++) {
+				console.log('adding')
+				var carousel = document.getElementById('events-container')
+				var event = createUpcomingEvent(upcomingEvents[currentEvent])
+				if (currentEvent == 0) {
+					event.className += " active"
+				}
+				carousel.appendChild(event)
+			}
 		}
 
 		if (hash == "socialForm") {
@@ -26,6 +122,8 @@ function replaceFragment(hash) {
 				beautifyForm()
 			})
 		}
+
+		window.scrollTo(0,0)
 	});
 }
 
---
+++ websiteData.js
@@ -0,0 +1,61 @@
+var announcements = [
+	{
+		title: {
+			link: "#socialForm",
+			text: "Dress Like a Tourist Day Submission"
+		},
+		eventInfo: [
+			"Homeroom Reps, please fill out this form by Tuesday at 9:00 PM"
+		]
+	},
+	{
+		title: "Homeroom Rep Meeting",
+		eventInfo: [
+			"This Wednesday"
+		]
+	},
+	{
+		title: {
+			link: "#hrCompResults",
+			text: "Paper Plane Toss Results!"
+		},
+		eventInfo: [
+			"First Place: 10 Points",
+			"Second Place: 8 Points",
+			"Third Place: 7 Points"
+		]
+	},
+	{
+		title: {
+			link: "#magDrive",
+			text: "Magazine Drive"
+		},
+		eventInfo: [
+			{
+				title: "What:",
+				text: "The premiere fundraising event for Miller Middle School"
+			}, {
+				title: "When:",
+				text: "Kickoff August 27"
+			}
+		]
+	}
+]
+
+var upcomingEvents = [
+	{
+		img: "http://www.ndhs.org/s/1012/images/editor/parents/top-magazines1.jpg",
+		title: {
+			link: "#magDrive",
+			text: "Magazine Drive"
+		},
+		info: "Kickoff Aug. 27"
+	},
+	{
+		img: "http://www.mahdloyz.org/wp-content/uploads/Paper-plane.jpg",
+		title: {
+			link: "#hrCompResults",
+			text: "Paper Plane Toss Results!"
+		}
+	}
+]
