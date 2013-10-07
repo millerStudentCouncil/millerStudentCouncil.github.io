84ce0799e7879cfcf87cf8c229be5191c4a24e4d
president <president>
2013-10-07 15:47:28 -0700

    update

--- websiteData.js
+++ websiteData.js
@@ -1,57 +1,33 @@
 var announcements = [
 	{
-		"title": {
-			"link": "http://tinyurl.com/btsbfeedback",
-			"text": "Back to School Bash Feedback"
-		},
-		"eventInfo": []
-	},
-	{
-		"title": "Pink Day",
+		"title": "Miller TechTalk: What does it take to build a company?",
 		"eventInfo": [
 			{
 				"title": "When:",
-				"text": "This Friday"
+				"text": "This Friday, October 11, from 3:15-4:15"
 			},
 			{
-				"title": "Rules:",
-				"text": "Wear one article of mostly pink clothing or at least 2 pink accessories."
+				"title": "Where:",
+				"text": "MP Room"
 			},
 			{
-				"title": "Borrowing Policy:",
-				"text": "A student borrowing the item from another person must request the item specifically from them before the spirit day.  A student or teacher may not bring extra items for people in their classroom who have not requested it specifically from them. If we catch students or teachers doing this, that homeroom will not recieve that week’s points."
+				"title": "Who:",
+				"text": "Krishna Vedati, CEO of Tynker"
 			}
 		]
 	},
-	{
-		title: {
-			link: "#hrCompResults",
-			text: "Paper Plane Toss Results!"
-		},
-		eventInfo: [
-			"First Place: 10 Points",
-			"Second Place: 8 Points",
-			"Third Place: 7 Points"
-		]
-	}
+
 ]
 
 var upcomingEvents = [
 	{
-	"img": "http://www.ballsnbands.com/images/Totemaster%20Rack.jpg ",
-	"title": "Ball Bin",
-	"info": "The ball bin will be opening on Monday, 10/7 during brunch and lunch! Remember to bring your ID card to check out basketballs, soccer balls, and footballs!"
+		"img": "http://business.utsa.edu/Business_News/b-update/images/entrepreneurship.jpg",
+		"title": "Miller TechTalk: What does it take to build a company?",
+		"info": "This Friday from 3:15-4:15 in the MP Room"
 	},
 	{
-		"img": "http://www.dayofpink.org/sites/all/themes/dayofpink/logo.png",
-		"title": "Pink Day",
-		"info": "This Friday"
-	},
-	{
-		img: "http://www.mahdloyz.org/wp-content/uploads/Paper-plane.jpg",
-		title: {
-			link: "#hrCompResults",
-			text: "Paper Plane Toss Results!"
-		}
+		"img": "http://www.ballsnbands.com/images/Totemaster%20Rack.jpg ",
+		"title": "Ball Bin",
+		"info": "The ball bin will be opening on Monday, 10/7 during brunch and lunch! Remember to bring your ID card to check out basketballs, soccer balls, and footballs!"
 	}
 ]
