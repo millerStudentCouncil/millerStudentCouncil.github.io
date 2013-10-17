4502d4c580eadbc4a33c17c6441e2740d2189810
president <president>
2013-10-17 16:28:58 -0700

    update

--- websiteData.js
+++ websiteData.js
@@ -1,5 +1,13 @@
 var announcements = [
 	{
+		"img": "Neon Day",
+		"title": {
+			"link": "http://introdekatelyn.files.wordpress.com/2011/07/neon-colors.jpg",
+			"text": "This is a fun day, not for homeroom points! Wear your neon clothes and remember to go to the glow dance!"
+		},
+		"info": ""
+	},
+	{
 		"title": {
 			"link": "https://docs.google.com/forms/d/1twcafgf70gvqD57wG60KcH-7q777ocVcC3Vi8wH53aQ/viewform",
 			"text": "Walk/Bike to School Week Results"
