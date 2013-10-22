91b80604ed6c4ac098d6b12e585567a87843ac99
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2013-10-22 12:19:13 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -1,5 +1,18 @@
 var announcements = [
 	{
+		"title": "Stuffed Animal Day",
+		"eventInfo": [
+			{
+				"title": "When:",
+				"text": "This Friday, 10/25"
+			},
+			{
+				"title": "What:",
+				"text": "Bring a stuffed animal larger than the hand"
+			}
+		]
+	},
+	{
 		"title": {
 			"link": "https://docs.google.com/forms/d/1twcafgf70gvqD57wG60KcH-7q777ocVcC3Vi8wH53aQ/viewform",
 			"text": "Walk/Bike to School Week Form"
@@ -18,6 +31,11 @@ var announcements = [
 ]
 
 var upcomingEvents = [
+	{
+		"img": "http://www.wadleighlibrary.org/wp-content/uploads/2012/06/stuffed-animals.jpg",
+		"title": "Stuffed Animal Day",
+		"info": "Bring a stuffed animal to school this Friday!"
+	},
 	{
 		"img": "http://www.ballsnbands.com/images/Totemaster%20Rack.jpg ",
 		"title": "Ball Bin",
