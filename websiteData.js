7c46adbe62dcfd54fb8bdaa18ff7cc5d428ae8e2
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2013-10-24 12:13:47 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -11,6 +11,22 @@ var announcements = [
 				"text": "Bring a stuffed animal larger than the hand"
 			}
 		]
+	},
+	{
+		"title": {
+			"link": "https://docs.google.com/forms/d/1XCMJJFhtE7dbvu5nmQrr5dqAN013eIYYCdYapsq1Tag/viewform",
+			"text": "Miller 5v5 Baseball Tournament"
+		},
+		"eventInfo": [
+			{
+				"title": "About:",
+				"text": "Please submit a response if you are interested in participating in this tournament. Only the first 16 teams of 5 students each to sign up will be competing in this tournament. All grades are welcome. The tournament days will be on 10/29, 10/30, 11/5, 11/6, 11/7, and the teachers vs. students game will be on 11/8 available to the winning team of the student tournament. The final schedule will be posted at the office on Monday 10/28."
+			},
+			{
+				"title": "Link for Signups:",
+				"text": "https://docs.google.com/forms/d/1XCMJJFhtE7dbvu5nmQrr5dqAN013eIYYCdYapsq1Tag/viewform"
+			}
+		]
 	}
 ]
 
