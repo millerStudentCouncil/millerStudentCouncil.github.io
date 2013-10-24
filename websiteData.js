f8d61bb1834e2c0d4f2545451229173caf6097c4
shadaj <shadaj>
2013-10-24 16:38:43 -0700

    Added Terror Cycling

--- websiteData.js
+++ websiteData.js
@@ -27,6 +27,23 @@ var announcements = [
 				"text": "https://docs.google.com/forms/d/1XCMJJFhtE7dbvu5nmQrr5dqAN013eIYYCdYapsq1Tag/viewform"
 			}
 		]
+	},
+	{
+		"title": "TERROR-Cycling!",
+		"eventInfo": [
+			{
+				"title": "Collection will be November 4th - 15th. ",
+				"text": "Turn in boxes will be in the main office and by the student store. DO NOT store them in your homeroom class. Bring them to the collection boxes labelled \"TerraCycle Boxes.\""
+			},
+			{
+				"title": "What to bring:",
+				"text": "Individual candy packs and giant multi-pack bags!"
+			},
+			{
+				"title": "Wait.. what is Terror-Cycling?",
+				"text": "Terror-Cycling is the name for Miller's kick off work with TerraCycle, a company that takes un-recyclable trash and creates consumer products from them. They take trash and make things like pouches and bags out of them! (PS - We make money when we send in these wrappers! For every $500 we earn, the following dance will have extra features.)"
+			}
+		]
 	}
 ]
 
