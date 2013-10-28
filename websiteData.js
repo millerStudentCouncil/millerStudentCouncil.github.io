da3d68c4caa3ecf71b3dc70e205f6d7d93abfae0
president <president>
2013-10-27 18:46:49 -0700

    update

--- websiteData.js
+++ websiteData.js
@@ -36,11 +36,6 @@ var announcements = [
 
 var upcomingEvents = [
 	{
-		"img": "http://www.wadleighlibrary.org/wp-content/uploads/2012/06/stuffed-animals.jpg",
-		"title": "Stuffed Animal Day",
-		"info": "Bring a stuffed animal to school this Friday!"
-	},
-	{
 		"img": "http://www.ballsnbands.com/images/Totemaster%20Rack.jpg ",
 		"title": "Ball Bin",
 		"info": "Remember to bring your ID card to check out basketballs, soccer balls, and footballs!"
