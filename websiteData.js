62d16fa27cd14083d9214c4745035dbd2bf8b191
president <president>
2013-12-03 19:09:42 -0800

    update

--- websiteData.js
+++ websiteData.js
@@ -1,18 +1,21 @@
 var announcements = [
 	{
-		"title": "Typhoon Relief Fund",
+		"title": {
+			"link": "tinyurl.com/millerdodgeball2013",
+			"text": "Miller Dodgeball Tournament Sign-ups"
+		},
+		"eventInfo": []
+	},
+	{
+		"title": "Winter Day",
 		"eventInfo": [
 			{
-				"title": "Due Date:",
-				"text": "THIS FRIDAY"
+				"title": "When:",
+				"text": "12/13"
 			},
 			{
-				"title": "What to bring:",
-				"text": "Clothing, toiletries, blankets, socks, and canned goods"
-			},
-			{
-				"title": "Where to turn it in:",
-				"text": "Room 33"
+				"title": "What Counts:",
+				"text": "<ul><li>Colors of white or blue</li><li>accessories include: mittens/gloves, earmuffs, scarves, beanies, etc.</li><li>1 article of clothing or two accessories</li></ul>"
 			}
 		]
 	},
@@ -24,70 +27,20 @@ var announcements = [
 	},
 	{
 		"title": {
-			"link": "https://docs.google.com/forms/d/1G0EKoWIz_HlCvAh77PTvhXqZWHKEg8IssmPONDoR33Q/viewform",
-			"text": "Walk/Bike to school week results form (Nov. 12-15)"
-		},
-		"eventInfo": []
-	},
-	{
-		"title": "We Scare Hunger",
-		"eventInfo": [
-			{
-				"title": "What:",
-				"text": "A food drive in participation with Second Harvest Food Bank"
-			},
-			{
-				"title": "When:",
-				"text": "10/24 to 11/20"
-			},
-			{
-				"title": "Where:",
-				"text": "Collection bins in the office and MP Room"
-			},
-			{
-				"title": "What to bring:",
-				"text": "Bottled water, cereal boxes, canned fruit, and baby food"
-			}
-		]
-	},
-	{
-		"title": {
 			"link": "https://docs.google.com/spreadsheet/ccc?key=0AiDXVDFiOJfadDI5bEYtVk1abWtwUk81TWJGUnM1clE&usp=sharing#gid=0",
 			"text": "Homeroom Competition Results"
 		},
 		"eventInfo": []
-	},
-	{
-		"title": {
-			"link": "#terracycle",
-			"text": "TERROR-Cycling!"
-		},
-		"eventInfo": [
-			{
-				"title": "Collection will be November 4th - 15th. ",
-				"text": "Turn in boxes will be in the main office and by the student store. DO NOT store them in your homeroom class. Bring them to the collection boxes labelled \"TerraCycle Boxes.\""
-			},
-			{
-				"title": "What to bring:",
-				"text": "Individual candy packs and giant multi-pack bags!"
-			},
-			{
-				"title": "Wait.. what is Terror-Cycling?",
-				"text": "Terror-Cycling is the name for Miller's kick off work with TerraCycle, a company that takes un-recyclable trash and creates consumer products from them. They take trash and make things like pouches and bags out of them! (PS - We make money when we send in these wrappers! For every $500 we earn, the following dance will have extra features.)"
-			}
-		]
 	}
 ]
 
 var upcomingEvents = [
 	{
-		"img": "http://www.weday.com/files/2013/09/WSH-356x267.jpg",
-		"title": "We Scare Hunger",
-		"info": "Turn in non-perishable items to the office and MP Room!"
-	},
-	{
-		"img": "http://www.walkbiketoschool.org/sites/default/files/OfficialLogos/WBTSD_2inch_Color.jpg ",
-		"title": "Walk/Bike to school week: November 12-15",
-		"info": "Remember: carpool is not accepted. Drop-off points are: Calabazas, Johnson and Bollinger, Prospect and Bollinger, and Safeway. "
+		"img": "http://static2.wikia.nocookie.net/__cb20131014035006/clubpenguin/images/7/7d/Dodgeball_Pin.PNG",
+		"title": {
+			"link": "tinyurl.com/millerdodgeball2013",
+			"text": "Dodgeball Tournament"
+		},
+		"info": ""
 	}
 ]
