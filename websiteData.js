9bd92ecfaad9e90efd67ec39b779207a8424c7bc
president <president>
2013-12-04 17:29:04 -0800

    update

--- websiteData.js
+++ websiteData.js
@@ -1,7 +1,28 @@
 var announcements = [
 	{
+		"title": "Miller TechTalks #2",
+		"eventInfo": [
+			{
+				"title": "When:",
+				"text": "THIS FRIDAY from 3:15 - 4:15"
+			},
+			{
+				"title": "Where:",
+				"text": "MP Room"
+			},
+			{
+				"title": "Speaker:",
+				"text": "Arun Gupta, Director of Developer Advocacy at Red Hat"
+			},
+			{
+				"title": "About:",
+				"text": "Minecraft is a multiplayer game about placing blocks in a 3-D environment, includes modifications (known as “mods”) written in Java that can alter the game. This session will share how I taught my son to build a mod. I'll share some tips on getting started with modding. I will also share how he got started with Scratch and Greenfoot which helped him pick up Java concepts easily. You will will learn a path that can be followed to start programming in Java, that you can enjoy and proudly share with friends. I'll also share information about upcoming workshops."
+			}
+		]
+	},
+	{
 		"title": {
-			"link": "tinyurl.com/millerdodgeball2013",
+			"link": "http://tinyurl.com/millerdodgeball2013",
 			"text": "Miller Dodgeball Tournament Sign-ups"
 		},
 		"eventInfo": []
