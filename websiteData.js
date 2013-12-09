92bf9548deed04dda4a46c9559672d057c085641
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2013-12-09 12:33:51 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -1,26 +1,5 @@
 var announcements = [
 	{
-		"title": "Miller TechTalks #2",
-		"eventInfo": [
-			{
-				"title": "When:",
-				"text": "THIS FRIDAY from 3:15 - 4:15"
-			},
-			{
-				"title": "Where:",
-				"text": "MP Room"
-			},
-			{
-				"title": "Speaker:",
-				"text": "Arun Gupta, Director of Developer Advocacy at Red Hat"
-			},
-			{
-				"title": "About:",
-				"text": "Minecraft is a multiplayer game about placing blocks in a 3-D environment, includes modifications (known as “mods”) written in Java that can alter the game. This session will share how I taught my son to build a mod. I'll share some tips on getting started with modding. I will also share how he got started with Scratch and Greenfoot which helped him pick up Java concepts easily. You will will learn a path that can be followed to start programming in Java, that you can enjoy and proudly share with friends. I'll also share information about upcoming workshops."
-			}
-		]
-	},
-	{
 		"title": {
 			"link": "http://tinyurl.com/millerdodgeball2013",
 			"text": "Miller Dodgeball Tournament Sign-ups"
@@ -56,6 +35,11 @@ var announcements = [
 ]
 
 var upcomingEvents = [
+	{
+		"img": "http://www.candywarehouse.com/assets/item/regular/candy-sticks-single-peppermint-126893_im.jpg",
+		"title": "Candy Cane Grams",
+		"info": "Candy Cane Grams are now available in the student store!"
+	},
 	{
 		"img": "http://static2.wikia.nocookie.net/__cb20131014035006/clubpenguin/images/7/7d/Dodgeball_Pin.PNG",
 		"title": {
