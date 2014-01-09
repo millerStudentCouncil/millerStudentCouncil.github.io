b788817eaa8c7877d001883e6a473918bec49301
president <president>
2014-01-08 19:19:29 -0800

    update

--- websiteData.js
+++ websiteData.js
@@ -15,13 +15,6 @@ var announcements = [
 	},
 	{
 		"title": {
-			"link": "#dodgeballTournament",
-			"text": "Miller Dodgeball Tournament Info"
-		},
-		"eventInfo": []
-	},
-	{
-		"title": {
 			"link": "https://docs.google.com/forms/d/1dEf9_2ZrnaCrHStgUmLHJ_Ay0pBZlO9uFjbOqbfPlFI/viewform",
 			"text": "Sign up for Chain Reaction's Volleyball Tournament!"
 		}
@@ -36,11 +29,6 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{
-		"img": "http://www.candywarehouse.com/assets/item/regular/candy-sticks-single-peppermint-126893_im.jpg",
-		"title": "Candy Cane Grams",
-		"info": "Candy Cane Grams are now available in the student store!"
-	},
 	{
 		"img": "http://static2.wikia.nocookie.net/__cb20131014035006/clubpenguin/images/7/7d/Dodgeball_Pin.PNG",
 		"title": {
