7cc9928ad272ff696b310f6e8b0f28250656e6a4
president <president>
2014-01-16 19:27:12 -0800

    Beautiful Forms

--- formStyling.js
+++ formStyling.js
@@ -1,3 +1,5 @@
+var submitted = false
+
 function beautifyForm () {
 	$('#ss-submit').addClass("btn btn-success btn-lg")
 	$(".ss-choices").css("list-style", "none")
@@ -11,7 +13,6 @@ function beautifyForm () {
 	$(".ss-required-asterisk").css("display", "none")
 	$(".aria-only-help").css("display", "none")
 	$(".ss-secondary-text").addClass("help-block")
-	$("#ss-form").attr("target", "_new")
 	$(".ss-q-short").addClass("form-control")
 	$(".ss-q-long").addClass("form-control")
 	$(".ss-q-other").addClass("form-control")
@@ -26,4 +27,15 @@ function beautifyForm () {
 
 	$("select").addClass("form-control")
 	$("select").prop("multiple", true)
+}
+
+function formSubmit () {
+	if (submitted == true) {
+		replaceFragment("main")
+		var alertObject = document.createElement("div")
+		alertObject.className = "alert alert-success alert-dismissable";
+		alertObject.innerHTML = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button> Your spirit day results have been submitted"
+		document.getElementById('alertHolder').appendChild(alertObject)
+		submitted = false
+	}
 }
