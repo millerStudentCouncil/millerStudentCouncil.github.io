7fd3a48af80f12ef6bbe2512665e7c6b27d7a695
president <president>
2014-01-23 16:31:11 -0800

    update

--- websiteData.js
+++ websiteData.js
@@ -1,9 +1,40 @@
 var announcements = [
 	{
+		"title": "Color Day! Wear your grade's color!",
+		"eventInfo": [
+			{
+				"title": "8th Grade",
+				"text": "Blue"
+			},
+			{
+				"title": "7th Grade",
+				"text": "Purple"
+			},
+			{
+				"title": "6th Grade",
+				"text": "Red"
+			},
+			{
+				"title": "Teachers",
+				"text": "Miller Apparel"
+			}
+		]
+	},
+	{
 		"title": {
-			"link": "https://docs.google.com/forms/d/1g2TECDHVV5Msug44hFh6Q8k8f_lHIjzvzyONNooWR48/viewform",
-			"text": "HOMEROOM REPS: Vote for the next Spirit Day's color"
-		}
+			"link": "https://docs.google.com/forms/d/1G0EKoWIz_HlCvAh77PTvhXqZWHKEg8IssmPONDoR33Q/edit",
+			"text": "Walk/Bike to school week submission form"
+		},
+		"eventInfo": [
+			{
+				"title": "Date:",
+				"text": "January 27-30"
+			},
+			{
+				"title": "Rules:",
+				"text": "No carpool accepted. Drop off points include: Calabazas, Safeway, or Johnson/Bollinger. "
+			}
+		]
 	},
 	{
 		"title": {
@@ -13,32 +44,19 @@ var announcements = [
 		"eventInfo": [
 			"<iframe width=\"100%\" src=\"https://docs.google.com/a/shadaj.me/spreadsheet/pub?key=0AmgNkG8GIejRdFJEcVZTeEdmSF96R0pQTlJ0VnlmM0E&output=html\"></iframe>"
 		]
-	},
-	{
-		"title": {
-			"link": "http://tinyurl.com/milleridol2014",
-			"text": "Miller Idol Signups"
-		},
-		"eventInfo": []
 	}
 ]
 
 var upcomingEvents = [
 	{
-		"img": "http://bestclipartblog.com/clipart-pics/microphone-clip-art-2.jpg",
-		"title": {
-			"link": "http://tinyurl.com/milleridol2014",
-			"text": "Miller Idol Signups"
-		},
+		"img": "http://www.pandahat.com/cfolder/Pandahat/Library/Tree/Panda_Hats/PAN001_Deluxe_Panda_Hat.jpg ",
+		"title": "Panda hats are available at the Student Store for $5 for ONE week, from 1/27/14-1/31/14!",
 		"info": ""
 	},
 	{
-		"img": "http://static2.wikia.nocookie.net/__cb20131014035006/clubpenguin/images/7/7d/Dodgeball_Pin.PNG",
-		"title": {
-			"link": "#dodgeballTournament",
-			"text": "Dodgeball Tournament"
-		},
-		"info": "Tomorrow!"
+		"img": "http://b.vimeocdn.com/ts/863/937/86393702_640.jpg",
+		"title": "Walk/Bike to school week",
+		"info": "January 27-30. No carpool will be accepted."
 	}
 ]
 
