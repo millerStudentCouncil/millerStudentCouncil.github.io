c787cf7d6ad4dfc18bd2e8ee03aa63f603b7135e
millerStudentCouncil <millerstudentcouncil@Millers-MacBook-Air.local>
2014-02-06 12:29:09 -0800

    added graffiti dance promotion

--- websiteData.js
+++ websiteData.js
@@ -17,6 +17,20 @@ var announcements = [
 	}
 ]
 
+{
+	"title": {
+		"link": "https://docs.google.com/document/d/14oUBfaMrJJ4yz23YIDso45H3br6SCyri6krNRg9s2Vg/edit",
+		"text": "Graffiti Dance Promotion"
+	},
+	"eventInfo": [
+		{
+			"title": "Graffiti Dance Promotion",
+			"text": "Click on the link below to learn more about this event! "
+		},
+		""
+	]
+}
+
 var upcomingEvents = [
 	{
 		"img": "https://lh3.googleusercontent.com/-6cA8OkwU7Uk/UvD3yceZt7I/AAAAAAAAAMA/9yOLotUIvNQ/w506-h327/Graffiti.jpg",
