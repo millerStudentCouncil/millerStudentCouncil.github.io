1192e69a926858b4c17863572ced5764155c2c64
president <president>
2014-02-07 06:40:31 -0800

    update

--- websiteData.js
+++ websiteData.js
@@ -19,9 +19,19 @@ var announcements = [
 
 var upcomingEvents = [
 	{
+		"img": "http://1.bp.blogspot.com/-RXiFyPHoTks/T0ucL-Y93yI/AAAAAAAAAJM/5PK0ZE-r0HI/s1600/oldschool-brain-graffiti.jpg",
+		"title": "Graffiti Dance Promotion",
+		"info": "Today!"
+	},
+	{
 		"img": "https://lh3.googleusercontent.com/-6cA8OkwU7Uk/UvD3yceZt7I/AAAAAAAAAMA/9yOLotUIvNQ/w506-h327/Graffiti.jpg",
 		"title": "Graffiti Dance!",
 		"info": "This Friday!"
+	},
+	{
+		"img": "http://shakespeareinaction.files.wordpress.com/2013/01/clip-art-pink-heart-backgrounds-powerpoint.jpg",
+		"title": "Valentine's Day Grams",
+		"info": "Valentine's Day Grams will be available in the Student Store from 2/3/14 to 2/7/14. The prices will be 1 for $4, 2 for $7, and 3 for $9. Remember to stop by! "
 	}
 ]
 
