cde81e7d896117b8043ef24bb138d60610c89d5e
president <president>
2014-06-03 20:07:26 -0700

    update

--- websiteData.js
+++ websiteData.js
@@ -14,8 +14,7 @@ var announcements = [
 		"eventInfo": [
 			"<iframe width=\"100%\" src=\"https://docs.google.com/spreadsheet/pub?key=0AkgD6_B2NIHwdHRMUkxJTmN3R1FtOWE2NE8ySVBoZHc&output=html\"></iframe>"
 		]
-	},
-	{"title":{"link":"redcrossblood.org","text":"Miller Blood Drive"},"eventInfo":[{"title":"When:","text":"May 30th from 11 AM to 4 PM"},{"title":"Who:","text":"Your parents and any other adults (ages: 16+)"},{"title":"Why:","text":"1. When an adult donates blood on your behalf, you get a prize 2. Just an hour of your time can save up to 3 other peoples’ lives!"},{"title":"How:","text":"Make an appointment at redcrossblood.org with the code at JMMS."}]}
+	}
 ]
 
 var upcomingEvents = [
