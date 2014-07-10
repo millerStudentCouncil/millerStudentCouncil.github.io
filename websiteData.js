54a4290c51d88f1671390eb08ed787347000da86
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-07-10 14:10:25 -0700

    Update websiteData.js
    testing out the website

--- websiteData.js
+++ websiteData.js
@@ -9,7 +9,7 @@ var announcements = [
 	{
 		"title": {
 			"link": "#homeroomResults",
-			"text": "Third Quarter Homeroom Competition Results!"
+			"text": "Welcome back to school!"
 		},
 		"eventInfo": [
 			"<iframe width=\"100%\" src=\"https://docs.google.com/spreadsheet/pub?key=0AkgD6_B2NIHwdHRMUkxJTmN3R1FtOWE2NE8ySVBoZHc&output=html\"></iframe>"
@@ -18,7 +18,7 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://www.americasfinestsummercamps.com/blog/wp-contents/uploads/2014/04/summer.jpg","title":"No Upcoming Events","info":"Have a Great Summer!hags"}
+	{"img":"http://www.floridadj.org/images/schooldance.jpg","title":"First Dance!","info":"Welcome back to school dance"}
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
