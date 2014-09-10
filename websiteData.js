4f7655ff0bb71627f5375d49e1726693d3297155
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-09-10 12:56:44 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -22,7 +22,7 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://www.gilmerfreepress.net/images/upload1/BacktoSchoolBashChurch.jpg","title":"Back to school bash!","info":"Rock wall! Bouncy Castle! food!"},
-	{"img":"http://videos.videopress.com/smZD9BmE/movie-night_dvd.original.jpg","title":"Outdoor Movie night","info":"Sept13 6;45"}
+	{"img":"http://videos.videopress.com/smZD9BmE/movie-night_dvd.original.jpg","title":"Outdoor Movie night","info":"September 13 7:15"}
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
