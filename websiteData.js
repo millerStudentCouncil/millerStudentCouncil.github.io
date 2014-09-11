0cecd02e829b728b80d5dc8e7a5954c5791bb81b
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-09-11 11:59:56 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -13,13 +13,7 @@ var announcements = [
 		},
 		"eventInfo": []
 	},
-	{
-		"title": {
-			"link": "#spritDay",
-			"text": "Nerd Day"
-		},
-		"eventInfo": [Submit Results]
-	},
+
 	
 	
 	
@@ -31,5 +25,5 @@ var upcomingEvents = [
 	{"img":"http://videos.videopress.com/smZD9BmE/movie-night_dvd.original.jpg","title":"Outdoor Movie night","info":"September 13 7:15"}
 ]
 
-var spiritDay = "" //Leave empty when there is no spirit day
-  
+var spiritDay = "Nerd Day" //Leave empty when there is no spirit day
+ 
