cd4582b0a872b97537fd9203f85bfb63c8bc8977
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-09-29 22:05:30 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -4,7 +4,14 @@ var announcements = [
 	{
 		"title": {
 			"link": "#OMN",
-			"text": "Outdoor Movie Night 2!"
+			"text": "Outdoor Movie Night"
+		},
+		"eventInfo": []
+	}
+	{
+		"title": {
+			"link": "#FaceTheCookieRules",
+			"text": "Face the Cookie"
 		},
 		"eventInfo": []
 	}
@@ -13,7 +20,8 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"The Second Outdoor movie night is coming up!","info":"Vote for your favorite movie!"}
+	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"The Second Outdoor Movie is almost here!","info":"October 3rd, 7:30-9:30"}
+	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is this Friday!","info":"October 3rd, in the quad at lunch"}
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
