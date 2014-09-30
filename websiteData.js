dc48f8e2181779823cfd0a7da092572b51c30304
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-09-29 21:21:01 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -16,5 +16,5 @@ var upcomingEvents = [
 	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"The Second Outdoor movie night is coming up!","info":"Vote for your favorite movie!"}
 ]
 
-var spiritDay = "Fandom Day" //Leave empty when there is no spirit day
+var spiritDay = "" //Leave empty when there is no spirit day
  
