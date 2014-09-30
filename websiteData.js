7d60b6553a19266ff407cdb9c9a032ab8810d00f
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-09-29 22:12:24 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -15,13 +15,13 @@ var announcements = [
 			"text": "Face the Cookie"
 		},
 		"eventInfo": []
-	}
+	},
 
 
 ]
 
 var upcomingEvents = [
-	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"The Second Outdoor Movie is almost here!","info":"October 3rd, 7:30-9:30"}
+	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"The Second Outdoor Movie is almost here!","info":"October 3rd, 7:30-9:30"},
 	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is this Friday!","info":"October 3rd, in the quad at lunch"}
 ]
 
