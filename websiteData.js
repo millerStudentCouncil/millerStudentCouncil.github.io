a61e9be4699ea7bf7d7b74c51c7df3c53d5a96bc
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-10-01 16:25:29 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -37,7 +37,7 @@ var announcements = [
 var upcomingEvents = [
 	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"","link":"http://millersc.org/#OMN","info":"October 3rd, 7:30-9:30 (In the gym)","link":"http://millersc.org/#IMN","title":"The first Indoor Movie Night is almost here!",},
 	{"img":"http://www.tiptopsigns.com/images/T/got_mustang_tn.jpg","title":"Miller Spirit Day is this Friday!","info":"October 3rd, wear Miller colors!"},
-	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is on October 17th!","info":"Elect a representative and meet in the quad at lunch!"}
+	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is on October 10th!","info":"Elect a representative and meet in the quad at lunch!"}
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
