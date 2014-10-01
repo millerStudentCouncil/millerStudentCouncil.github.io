74ef1d1a306da6b84d5b8998c870d20b6571c988
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-10-01 16:31:52 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -36,7 +36,7 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"","link":"http://millersc.org/#OMN","info":"October 3rd, 7:30-9:30 (In the gym)","link":"http://millersc.org/#IMN","title":"The first Indoor Movie Night is almost here!",},
-	{"img":"http://www.tiptopsigns.com/images/T/got_mustang_tn.jpg","title":"Got Mustang?","info":"This Friday, October 3rd, show your Miller Spirit and wear black and yellow or miller attire!"},
+	{"img":"http://www.tiptopsigns.com/images/T/got_mustang_tn.jpg","title":"Got Mustang?","info":"This Friday, October 3rd, show your Miller Spirit and wear black and yellow or Miller attire!"},
 	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is on October 10th!","info":"Elect a representative and meet in the quad at lunch!"},
 	{"img":"http://cdn.buzznet.com/assets/users16/peaceluvgirl205/default/80s-90s-fashion-back-style--large-msg-131344177598.jpg","title":"80's and 90's Spirit Day!","info":"Show your spirit on October 17th!"},
 
