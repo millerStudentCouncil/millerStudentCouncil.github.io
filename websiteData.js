5128f9fb16f1a7bc0c103f43be6e2713daac5a48
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-10-04 19:52:38 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -2,20 +2,6 @@ var announcements = [
 	
 
 	{
-		"title": {
-			"link": "#IMN",
-			"text": "Indoor Movie Night"
-		},
-		"eventInfo": []
-	},
-	{
-		"title": {
-			"link": "#millerSpiritDay",
-			"text": "Miller Spirit Day"
-		},
-		"eventInfo": []
-	},
-	{
 	
 		"title": {
 			"link": "#faceTheCookieRules",
@@ -42,10 +28,9 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://images.sodahead.com/polls/002390585/3834213572_popcorn_xlarge.jpeg","title":"","link":"http://millersc.org/#OMN","info":"October 3rd, 7:30-9:30 (In the gym)","link":"http://millersc.org/#IMN","title":"The first Indoor Movie Night is almost here!",},
-	{"img":"http://www.tiptopsigns.com/images/T/got_mustang_tn.jpg","title":"Got Mustang?","info":"This Friday, October 3rd, show your Miller Spirit and wear black and yellow or Miller attire!"},
 	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is on October 10th!","info":"Elect a representative and meet in the quad at lunch!"},
 	{"img":"http://cdn.buzznet.com/assets/users16/peaceluvgirl205/default/80s-90s-fashion-back-style--large-msg-131344177598.jpg","title":"80's and 90's Spirit Day!","info":"Show your spirit on October 17th!"},
+	{"img":"http://media1.giphy.com/media/M2Ml23remyvjq/giphy.gif","title":"Come to the spookiest dance of the year on October 17th!","info":"The dance is from 7:00-9:30. Pizza, churros, candy, Pearl Milk Tea, and other delicious food will be sold!"},
 
 ]
 
