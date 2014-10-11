bbfa28d166ec9b76d7824436444c4bfce3dee559
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-10-10 17:41:27 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -2,14 +2,6 @@ var announcements = [
 	
 
 	{
-	
-		"title": {
-			"link": "#faceTheCookieRules",
-			"text": "Face the Cookie"
-		},
-		"eventInfo": []
-	},
-	{
 		"title": {
 			"link": "#80s90sSpiritDay",
 			"text": "80's and 90's Spirit Day"
@@ -28,7 +20,6 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://www.bariatricafterlife.com/wp-content/uploads/2011/07/eat_me.jpg","title":"Face the Cookie is on October 10th!","info":"Elect a representative and meet in the quad at lunch!"},
 	{"img":"http://cdn.buzznet.com/assets/users16/peaceluvgirl205/default/80s-90s-fashion-back-style--large-msg-131344177598.jpg","title":"80's and 90's Spirit Day!","info":"Show your spirit on October 17th!"},
 	{"img":"http://media1.giphy.com/media/M2Ml23remyvjq/giphy.gif","title":"Come to the spookiest dance of the year on October 17th!","info":"The Fright Night Dance is from 7:00-9:30. Pizza, churros, candy, Pearl Milk Tea, and other delicious food will be sold!"},
 
