a304794d3809d0b1bf97221f57b347cf81f3c927
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-10-20 18:47:21 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -33,8 +33,6 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://cdn.buzznet.com/assets/users16/peaceluvgirl205/default/80s-90s-fashion-back-style--large-msg-131344177598.jpg","title":"80's and 90's Spirit Day!","info":"Show your spirit on October 17th!"},
-	{"img":"http://media1.giphy.com/media/M2Ml23remyvjq/giphy.gif","title":"Come to the spookiest dance of the year on October 17th!","info":"The Fright Night Dance is from 7:00-9:30. Pizza, churros, candy, Pearl Milk Tea, and other delicious food will be sold!"},
 	{"img":"http://fc06.deviantart.net/fs16/i/2007/217/4/a/Movie_Night_by_jpox.jpg","title":"Did you go to the last Movie Night on October 3rd?","info":"If so, please help Leadership plan an even better Movie Night by filling out this short survey!"},
 	{"img":"http://www.candywarehouse.com/assets/item/regular/warheads-logo-127337.jpg","title":"The Sour Candy Challenge is coming up this Thursday!","info":"In the Quad at Lunch, October 23rd!"},
 	{"img":"http://www.weday.com/files/2012/08/WSH.jpg","title":"Help families in poverty by bringing donation non-perishable food items!","info":"Trick-or-Treat for canned goods instead of candy!"},
