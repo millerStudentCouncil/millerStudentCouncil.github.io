c08982f7c286d679796bc0a1c5e9c79e7b5a3461
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-10-27 19:01:45 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -10,13 +10,6 @@ var announcements = [
 	{
 		
 		"title": {
-			"link": "#warheadChallenge",
-			"text": "Warhead Sour Candy Challenge"
-		},
-	},
-	{
-		
-		"title": {
 			"link": "#weScareHunger",
 			"text": "We Scare Hunger"
 		},
@@ -34,9 +27,8 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://fc06.deviantart.net/fs16/i/2007/217/4/a/Movie_Night_by_jpox.jpg","title":"Did you go to the last Movie Night on October 3rd?","info":"If so, please help Leadership plan an even better Movie Night by filling out this short survey!"},
-	{"img":"http://www.candywarehouse.com/assets/item/regular/warheads-logo-127337.jpg","title":"The Sour Candy Challenge is coming up this Thursday!","info":"In the Quad at Lunch, October 23rd!"},
 	{"img":"http://www.weday.com/files/2012/08/WSH.jpg","title":"Help families in poverty by bringing donation non-perishable food items!","info":"Trick-or-Treat for canned goods instead of candy!"},
-	{"img":"http://morethanthecurve.com/wp-content/uploads/2013/10/Halloween-Parade-Cartoon-631x420.jpg","title":"Participate in the Halloween Parade on October 31st!","info":"Remember... No masks, weaponry, or anything that does not follow the dress code!"},
+	{"img":"http://morethanthecurve.com/wp-content/uploads/2013/10/Halloween-Parade-Cartoon-631x420.jpg","title":"Participate in the Halloween Parade or the Halloween Spirit Day on October 31st!","info":"Remember... No masks, weaponry, or anything that does not follow the dress code! If you do not want to dress up in a costume, simply wear both black and orange!"},
 
 
 ]
