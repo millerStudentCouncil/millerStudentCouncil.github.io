d60d02193a4cf61737d53722ee7cfe99d8f5e51b
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2014-12-26 15:18:57 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -3,32 +3,14 @@ var announcements = [
 
 	{
 		"title": {
-			"link": "#lightItUpDay",
-			"text": "To promote the Light Up the Night Dance, wear Neon on Friday!"
-		},
-	},
-	{
-		"title": {
-			"link": "#lightUpTheNightDance",
-			"text": "The Light Up the Night Dance is this Friday!"
-		},
-	},
-	{
-		"title": {
-			"link": "tinyurl.com/lutnsongs",
-			"text": "Request Songs for the Light Up the Night Dance!"
-		},
-	},
-		{
-		"title": {
 			"link": "http://tinyurl.com/omnthree",
 			"text": "Vote for what kind of movie you want at the next Movie Night!"
 		},
 	},
 	{
 		"title": {
-			"link": "#firstQuarterHomeroomPoints",
-			"text": "The First Quarter Homeroom Points are final!"
+			"link": "#pajamaDay",
+			"text": "Pajama Day on January 9th!"
 		},
 	},
 
@@ -36,12 +18,12 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store soon!","info":"Each rafiki is $8, with $5 going toward empowering Maasai Mamas in Africa!"},
-	{"img":"http://www.hdwallpapers-3d.com/wp-content/uploads/2014/09/cool-lights-hd-wallpaper-1.jpg","title":"The Light Up the Night Dance will be on December 12th!","info":"Prepare yourself for an amazing night with your friends that will be unforgettable!"},
+	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
+	{"img":"https://img0.etsystatic.com/032/0/6965312/il_fullxfull.541235214_d80j.jpg","title":"Do you ever just want to come to school in your pajamas?","info":"No need to change your clothes! Come to school and show your friends you cool PJ's!"},
 
 ]
 
-var spiritDay = "Light It Up Spirit Day" //Leave empty when there is no spirit day
+var spiritDay = "" //Leave empty when there is no spirit day
 
 var formHashes = ["techRequestForm","eventForm"] // Hashes to be formatted as google forms, DO NOT INCLUDE spiritDay
 var formHashes = ["songRequestForm","eventForm"] // Hashes to be formatted as google forms, DO NOT INCLUDE spiritDay
