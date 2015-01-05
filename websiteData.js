dbeea7b54b7ffbe86f26489be6272252cc967647
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-01-05 15:40:23 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -3,11 +3,6 @@ var announcements = [
 
 	{
 		"title": {
-			"text": "Earth to Echo will be playing at the next Movie Night!"
-		},
-	},
-	{
-		"title": {
 			"link": "#pajamaDay",
 			"text": "Pajama Day on January 9th!"
 		},
@@ -19,6 +14,7 @@ var announcements = [
 var upcomingEvents = [
 	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
 	{"img":"https://img0.etsystatic.com/032/0/6965312/il_fullxfull.541235214_d80j.jpg","title":"Do you ever just want to come to school in your pajamas?","info":"No need to change your clothes! Come to school and show your friends you cool PJ's!"},
+	{"img":"http://www.impawards.com/2014/posters/earth_to_echo_ver3_xlg.jpg","title":"Get ready for a night of adventures with Echo!","info":"Join your friends on January 23rd for an Indoor Movie Night!"},
 
 ]
 
