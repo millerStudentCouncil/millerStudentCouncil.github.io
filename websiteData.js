6311df129bcde0fbe13408615712551d1747312f
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-01-08 16:21:35 -0800

    Update websiteData.js
    maxwell wuz here
     also don't do these extremely long things on the carousel it messes it up

--- websiteData.js
+++ websiteData.js
@@ -27,7 +27,7 @@ var upcomingEvents = [
 	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
 	{"img":"https://img0.etsystatic.com/032/0/6965312/il_fullxfull.541235214_d80j.jpg","title":"Do you ever just want to come to school in your pajamas?","info":"No need to change your clothes! Come to school and show your friends you cool PJ's!"},
 	{"img":"http://movieboozer.com/wp-content/uploads/2014/07/earth_to_echo_2014_movie-wide.jpg","title":"Get ready for a night of adventures with Echo!","info":"Join your friends on January 23rd for an Indoor Movie Night!"},
-	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Go to tinyurl.com/DodgeballTournamentSignUps to sign up your team!"},
+	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 
 ]
 
