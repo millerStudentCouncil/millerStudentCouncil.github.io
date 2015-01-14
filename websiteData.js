bb256fed64bef18a9d9fce4548b79bcfbf768fc2
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-01-14 15:20:12 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -3,20 +3,8 @@ var announcements = [
 
 	{
 		"title": {
-			"link": "https://docs.google.com/document/d/17igeuiYITeCmZFNgPDmZiI-XyAMXvCAp60qx065-4_w/edit?usp=sharing",
-			"text": "Pajama Day on January 9th!"
-		},
-	},
-	{
-		"title": {
-			"link": "http://millersc.org/#calendar",
-			"text": "Homeroom Representatives: The next Student Council Meeting is next Wednesday, January 14th!"
-		},
-	},
-	{
-		"title": {
 			"link": "http://millersc.org/#dodgeball",
-			"text": "Sign up here to play in the ANNUAL DODGEBALL TOURNAMENT!!! "
+			"text": "Sign up here to play in the ANNUAL DODGEBALL TOURNAMENT!!!"
 		},
 	},
 
@@ -25,13 +13,13 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
-	{"img":"https://img0.etsystatic.com/032/0/6965312/il_fullxfull.541235214_d80j.jpg","title":"Do you ever just want to come to school in your pajamas?","info":"No need to change your clothes! Come to school and show your friends you cool PJ's!"},
 	{"img":"http://movieboozer.com/wp-content/uploads/2014/07/earth_to_echo_2014_movie-wide.jpg","title":"Get ready for a night of adventures with Echo!","info":"Join your friends on January 23rd for an Indoor Movie Night!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
+	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 
 ]
 
-var spiritDay = "Pajama Day" //Leave empty when there is no spirit day
+var spiritDay = "" //Leave empty when there is no spirit day
 
 var formHashes = ["techRequestForm","techRequestForm"] // Hashes to be formatted as google forms, DO NOT INCLUDE spiritDay
 var formHashes = ["dodgeball","dodgeball"] // Hashes to be formatted as google forms, DO NOT INCLUDE spiritDay
