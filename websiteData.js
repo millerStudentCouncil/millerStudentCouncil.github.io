6646fbfeb5849dc6416ae6318823d1f738c1ca1d
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-01-15 12:04:45 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -40,6 +40,7 @@ var upcomingEvents = [
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 	{"img":"http://www.psula.org/images/article_images/316.jpg","title":"Rock out with your friends at the Hollywood Dance!","info":"February 6th (Friday), in the Quad from 7:00-9:30pm"
+	{"img":"http://king.jpschools.org/wp-content/uploads/sites/128/2014/11/Penny.png","title":"Compete against all grade levels in Miller's first Penny Wars Competition!","info":"The winning grade level receives a mini-dance party! Remember... Coins are positive and Dollar bills are negative!"},
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
