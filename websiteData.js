09ebb203f0e6ddbb93865edb03ce886c629b782d
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-02-01 18:27:50 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -48,7 +48,6 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
-	{"img":"http://movieboozer.com/wp-content/uploads/2014/07/earth_to_echo_2014_movie-wide.jpg","title":"Get ready for a night of adventures with Echo!","info":"Join your friends on January 23rd for an Indoor Movie Night!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 	{"img":"http://www.psula.org/images/article_images/316.jpg","title":"Rock out with your friends at the Hollywood Dance!","info":"February 6th (Friday), in the Quad from 7:00-9:30pm"},
