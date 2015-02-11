7bfce0a4859df29d70b9398ae60b79deee377152
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-02-11 15:52:49 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -15,12 +15,6 @@ var announcements = [
 	},
 	{
 		"title": {
-			"link": "tinyurl.com/hdsrequests",
-			"text": "Submit Song Requests for the Hollywood Dance!"
-		},
-	},
-	{
-		"title": {
 			"link": "www.schoola.com/",
 			"text": "Schoola Clothing Drive"
 		},
@@ -37,12 +31,7 @@ var announcements = [
 			"text": "Prepare yourselves for Miller's first ever Penny Wars!"
 		},
 	},
-	{
-		"title": {
-			"link":"#projectPurple",
-			"text":"Support Project Purple by wearing purple for a week!"
-		},
-	},
+
 
 ]
 
@@ -50,9 +39,7 @@ var upcomingEvents = [
 	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://www.psula.org/images/article_images/316.jpg","title":"Rock out with your friends at the Hollywood Dance!","info":"February 6th (Friday), in the Quad from 7:00-9:30pm"},
 	{"img":"http://king.jpschools.org/wp-content/uploads/sites/128/2014/11/Penny.png","title":"Compete against all grade levels in Miller's first Penny Wars Competition!","info":"The winning grade level receives a mini-dance party! Remember... Coins are positive and Dollar bills are negative!"},
-	{"img":"http://outerbanksdaredevils.com/wp-content/uploads/2013/06/aaa-project-purple-LOGO.jpg","title":"What's better than wearing purple all week? Nothing!","info":"Support Project Purple from January 26-29 by wearing purple each day!"},
 	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Fancy Dance Tickets sold in the Quad at lunch from February 2-March 13!  Buy an Early Bird ticket for $15 before February 27, or you will be required to pay $20 after that."},
 ]
 
