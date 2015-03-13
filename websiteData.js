bb75a73b63afeaa6714a4db7d0e28019d430b83e
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-03-13 12:31:37 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -43,25 +43,17 @@ var announcements = [
 			"text": "Did you always want to be a waiter or chef? Sign up to be one for the Fancy Dance!"
 		},
 	},
-	{
-		"title": {
-			"link": "#pennyWars",
-			"text": "Prepare yourselves for Miller's first ever Penny Wars!"
-		},
-	},
 
 
 ]
 
 var upcomingEvents = [
-	{"img":"http://www.freethechildren.com/wp-content/uploads/2014/09/rafikis_banner.jpg","title":"Rafikis from Free the Children will be sold at the Student Store!","info":"Each rafiki is $10, with $5 going toward empowering Maasai Mamas in Africa and $5 going to Free the Children!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
-	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://king.jpschools.org/wp-content/uploads/sites/128/2014/11/Penny.png","title":"Compete against all grade levels in Miller's first Penny Wars Competition!","info":"The winning grade level receives a mini-dance party! Remember... Coins are positive and Dollar bills are negative!"},
 	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Hurry up and buy your Fancy Dance tickets!","info":"Friday, March 13th, is the last day you can purchase a ticket!"},
 	{"img":"http://www.graphics18.com/wp-content/uploads/2010/03/happystpatdaygarfieldblinks.gif","title":"Don't get pinched... wear green!","info":"Tuesday, March 17"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
+	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
