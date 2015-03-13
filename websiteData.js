ba34f2ed5628baf740c3ba9135d0d2e09ca135de
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-03-13 11:55:23 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -9,6 +9,12 @@ var announcements = [
 	},
 	{
 		"title": {
+			"link": "#st.PatsDay",
+			"text": "Next Tuesday, wear green for St. Patrick's Day!"
+		},
+	},
+	{
+		"title": {
 			"link": "http://millersc.org/#dodgeball",
 			"text": "Sign up here to play in the annual Dodgeball Tournament!"
 		},
@@ -47,7 +53,7 @@ var upcomingEvents = [
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 	{"img":"http://king.jpschools.org/wp-content/uploads/sites/128/2014/11/Penny.png","title":"Compete against all grade levels in Miller's first Penny Wars Competition!","info":"The winning grade level receives a mini-dance party! Remember... Coins are positive and Dollar bills are negative!"},
 	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Hurry up and buy your Fancy Dance tickets!","info":"Friday, March 13th, is the last day you can purchase a ticket!"},
-	{"img":"http://www.graphics18.com/wp-content/uploads/2010/03/happystpatdaygarfieldblinks.gif","title":"Don't get pinched... wear green!","info":"Friday, March 20"},
+	{"img":"http://www.graphics18.com/wp-content/uploads/2010/03/happystpatdaygarfieldblinks.gif","title":"Don't get pinched... wear green!","info":"Tuesday, March 17"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 ]
 
