7948f118b7ca6e831cb0140405f4c4775108c82e
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-03-17 12:30:20 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -60,13 +60,13 @@ var announcements = [
 ]
 
 var upcomingEvents = [
+	{"img":"http://i.imgur.com/ecKsDgY.gif","title":"Baymax already has a lot of fans out there... Do you want to become one, too?","info":"Big Hero 6 will be shown at the next Indoor Movie Night in the MP Room!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Prepare yourself for a fun night with your fellow 8th grade classmates at the Fancy Dance!","info":"Friday, March 20th"},
 	{"img":"http://www.graphics18.com/wp-content/uploads/2010/03/happystpatdaygarfieldblinks.gif","title":"Don't get pinched... wear green!","info":"Tuesday, March 17"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Baymax already has a lot of fans out there... Do you want to become one, too?","info":"Big Hero 6 will be shown at the next Indoor Movie Night in the MP Room!"},
 ]
 
 var spiritDay = "St. Patrick's Day" //Leave empty when there is no spirit day
