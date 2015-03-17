61946fd636494ed98cd6afd183de0ee802cfa061
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-03-17 12:11:20 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -16,13 +16,7 @@ var announcements = [
 	{
 		"title": {
 			"link": "tinyurl.com/fancydanceinfo2015",
-			"text": "8th Graders: March 13th, this Friday, is the last day to buy fancy dance tickets! Hurry!"
-		},
-	},
-	{
-		"title": {
-			"link": "#st.PatsDay",
-			"text": "Next Tuesday, wear green for St. Patrick's Day!"
+			"text": "Fancy Dance Tickets anymore, but if you did buy a ticket, here is more information about the dance!"
 		},
 	},
 	{
@@ -61,7 +55,7 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
-	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Hurry up and buy your Fancy Dance tickets!","info":"Friday, March 13th, is the last day you can purchase a ticket!"},
+	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Prepare yourself for a fun night with your fellow 8th grade classmates at the Fancy Dance!","info":"Friday, March 20th"},
 	{"img":"http://www.graphics18.com/wp-content/uploads/2010/03/happystpatdaygarfieldblinks.gif","title":"Don't get pinched... wear green!","info":"Tuesday, March 17"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
