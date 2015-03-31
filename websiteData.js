93c52e661ab5d42efd4552f9ad319e050dfac8fd
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-03-30 19:50:26 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -42,7 +42,7 @@ var upcomingEvents = [
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://ezgif.com/save/ezgif-2180653862.gif","title":"Sign up for the Miller Oscars today!","info":"Team up with your friends to make an amazing movie! Go to the signup form to learn more!"},
+	{"img":"http://www.techfleece.com/wp-content/uploads/2015/02/The-Oscars-2015-logo.jpg","title":"Sign up for the Miller Oscars today!","info":"Team up with your friends to make an amazing movie! Go to the signup form to learn more!"},
 ]
 
 var spiritDay = "" //Leave empty when there is no spirit day
