2ed60868ac1d4e79697db4c7ee3df67bfb989924
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-03-30 19:42:08 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -22,26 +22,14 @@ var announcements = [
 	},
 	{
 		"title": {
-			"link": "tinyurl.com/fancydanceinfo2015",
-			"text": "Fancy Dance Tickets anymore, but if you did buy a ticket, here is more information about the dance!"
-		},
-	},
-	{
-		"title": {
 			"link": "#weAreSilent",
-			"text": "Prepare yourself for We Are Silent!"
+			"text": "Prepare yourselves for We Are Silent!"
 		},
 	},
 	{
 		"title": {
 			"link": "http://www.schoola.com/schools/joaquin-miller-middle-san-jose-ca",
 			"text": "Schoola Clothing Drive"
-		},
-	},
-	{
-		"title": {
-			"link": "#fancyDanceCooking",
-			"text": "Did you always want to be a waiter or chef? Sign up to be one for the Fancy Dance!"
 		},
 	},
 
@@ -51,14 +39,12 @@ var announcements = [
 var upcomingEvents = [
 	{"img":"http://i.imgur.com/ecKsDgY.gif","title":"Baymax already has a lot of fans out there... Do you want to become one, too?","info":"Big Hero 6 will be shown at the next Indoor Movie Night in the MP Room!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
-	{"img":"https://agirlandherdot.files.wordpress.com/2010/11/fancy-copy.jpg","title":"Prepare yourself for a fun night with your fellow 8th grade classmates at the Fancy Dance!","info":"Friday, March 20th"},
-	{"img":"http://www.graphics18.com/wp-content/uploads/2010/03/happystpatdaygarfieldblinks.gif","title":"Don't get pinched... wear green!","info":"Tuesday, March 17"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://ezgif.com/save/ezgif-2180653862.gif","title":"Sign up for the Miller Oscars today","info":"Team up with your friends to make an amazing movie! More info on the sign up sheet"},
+	{"img":"http://ezgif.com/save/ezgif-2180653862.gif","title":"Sign up for the Miller Oscars today!","info":"Team up with your friends to make an amazing movie! Go to the signup form to learn more!"},
 ]
 
-var spiritDay = "St. Patrick's Day" //Leave empty when there is no spirit day
+var spiritDay = "" //Leave empty when there is no spirit day
 
 var formHashes = ["techRequestForm", "dodgeball", "fancyDanceCooking","oscarForm"] // Hashes to be formatted as google forms, DO NOT INCLUDE spiritDay
