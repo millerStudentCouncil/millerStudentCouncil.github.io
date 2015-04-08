e41ef02007ec0a55b63c8253aca40b6ebcea3d10
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-04-07 18:18:45 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -5,13 +5,7 @@ var announcements = [
 	{
 		"title": {
 			"link": "#oscarForm",
-			"text": "Register for the annual Miller OSCARS!!!"
-		},
-	},
-	{
-		"title": {
-			"link": "#indoorMovieNight",
-			"text": "Big Hero 6 will be shown at the next Indoor Movie Night!"
+			"text": "Register for Miller Oscars before this Friday!"
 		},
 	},
 	{
@@ -37,7 +31,6 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	{"img":"http://i.imgur.com/ecKsDgY.gif","title":"Baymax already has a lot of fans out there... Do you want to become one, too?","info":"Big Hero 6 will be shown at the next Indoor Movie Night in the MP Room!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
