c41b67413e9517ba71f25a90c90228a00d0b2f01
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-04-24 12:37:43 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -25,11 +25,15 @@ var announcements = [
 ]
 
 var upcomingEvents = [
+	{"img":"https://kidology.org/store/images/products/real_fun/smarter_md.jpg","title":"Are you smarter than a teacher?","info":"Come watch your classmates in the quad at lunch on April 27th to April 30th and on May 5th!"},
+	{"img":"http://mountainparkes.org/wp-content/uploads/teacher-appreciation.jpg","title":"Give back to our wonderful teachers for Teacher's Appreciation Week!","info":"Secretly create a card with your homeroom!"},
+	{"img":"http://www.tccnh.org/wp-content/uploads/2013/07/Mothers_Day-1024x768.jpg","title":"Buy a mother's day gram (carnation) for your amazing mothers for only $5!","info":"In the Student Store from April 27th to May 7th!"},
+	{"img":"http://blogs.swa-jkt.com/swa/5l/files/2012/09/Broken-Egg-386x315.jpg","title":"The annual egg drop is on May 1st in front of the gym!","info":"Turn in your contraption to Room 14 during Brunch.  There are no EGG-ceptions!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"http://www.theurbanprince.com/wp-content/uploads/2012/03/cpwilsoniii-the-avengers-meet-winnie-the-pooh.jpeg","title":"What superhero or villian will you be dressing up as?","info":"Decide soon before the Comic Battle Dance on April 24th!"},
 	{"img":"https://natalieisgoode.files.wordpress.com/2014/04/hush2.jpg","title":"Join millions of people around the world and vow to be silent for We Are Silent!","info":"On Thursday, April 23rd, stay silent to support children around the world that are denied of their basic rights!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://www.techfleece.com/wp-content/uploads/2015/02/The-Oscars-2015-logo.jpg","title":"Sign up for the Miller Oscars today!","info":"Team up with your friends to make an amazing movie! Go to the signup form to learn more!"},
+	{"img":"http://www.techfleece.com/wp-content/uploads/2015/02/The-Oscars-2015-logo.jpg","title":"Oscar Night videos are due on May 8th!","info":"The winners of each category receive a cool trophy and other prizes!"},
 ]
 
 var spiritDay = "Superhero VS Supervillain Day" //Leave empty when there is no spirit day
