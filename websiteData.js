c6738b28169de63752357ea6faf7d134f23add00
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-05-12 12:05:36 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -9,12 +9,6 @@ var announcements = [
 	},
 	{
 		"title": {
-			"link":"tinyurl.com/twindayrule",
-			"text":"Twin Day is this Friday! Click here for the rules!"
-		},
-	},
-	{
-		"title": {
 			"link":"#newspaperFashionShow",
 			"text":"Compete with your friends in the Newspaper Fashion Show!"
 		},
@@ -33,12 +27,6 @@ var announcements = [
 	},	
 	{
 		"title": {
-			"link": "#terracycle",
-			"text": "Get ready for this year's Terracycle!"
-		},
-	},
-	{
-		"title": {
 			"link": "#oscar",
 			"text": "Oscar night is coming to Miller!"
 		},
@@ -49,11 +37,8 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://files.hgsitebuilder.com/hostgator705321/image/graduation-leis.jpg","title":"Purchase your flowers for graduation now!","info":"Visit kahunaflowers.com, choose Joaquin Miller M.S. as your school, and buy away!"},
-	{"img":"http://img1.wikia.nocookie.net/__cb20091230161218/mrmen/images/0/06/Twins_book.jpg","title":"Twin Day is tomorrow!","info":"Twin with your friends to show your school spirit and earn homeroom points!"},
 	{"img":"https://s-media-cache-ak0.pinimg.com/236x/51/e0/e9/51e0e9f1269d3ba62c3b6b56dbebc6ec.jpg","title":"Is your dream to become a world-famous clothes designer?","info":"Either way, compete in the Newspaper Fashion Show and show off your fashion skills!"},
 	{"img":"http://www.banthebottle.net/wp-content/uploads/2012/05/RecycleSymbol_wPlasticBottles.jpg","title":"The Plastic to Pennies Drive is coming soon!","info":"Recycle your empty plastic water bottles in exchange for pennies!"},
-	{"img":"http://mountainparkes.org/wp-content/uploads/teacher-appreciation.jpg","title":"Give back to our wonderful teachers for Teacher's Appreciation Week!","info":"Secretly create a card with your homeroom!"},
-	{"img":"http://www.tccnh.org/wp-content/uploads/2013/07/Mothers_Day-1024x768.jpg","title":"Buy a mother's day gram (carnation) for your amazing mothers for only $5!","info":"In the Student Store from April 27th to May 7th!"},
 	{"img":"https://akamai-activenet009.active.com/mammothrecreation/servlet/downloadFile.sdi?uploadedfile_id=140","title":"Compete against your fellow classmates in Miller's Dodgeball Tournament!","info":"Under SSC click Sign up to sign up your team!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 	{"img":"http://www.techfleece.com/wp-content/uploads/2015/02/The-Oscars-2015-logo.jpg","title":"Oscar Night videos are due on May 8th!","info":"The winners of each category receive a cool trophy and other prizes!"},
