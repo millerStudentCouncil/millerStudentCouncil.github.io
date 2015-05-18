295df205a60d2434555bb2c857a4549fc935d494
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-05-18 12:56:42 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -3,6 +3,12 @@ var announcements = [
 
 	{
 		"title": {
+			"link":"#lookAtTheBooks",
+			"text":"Learn more about Miller's first ever Look At the Books Drive!"
+		},
+	},
+	{
+		"title": {
 			"link":"kahunaflowers.com",
 			"text":"Attention 8th Graders! Order your graduation flowers for this year!"
 		},
@@ -36,6 +42,7 @@ var announcements = [
 ]
 
 var upcomingEvents = [
+	{"img":"http://files.hgsitebuilder.com/hostgator705321/image/graduation-leis.jpg","title":"The Look At the Books Drive is from May 20th until June 3rd!","info":"We are collecting _gently_ used books to help raise money for Nepal earthquake relief."},
 	{"img":"http://files.hgsitebuilder.com/hostgator705321/image/graduation-leis.jpg","title":"Purchase your flowers for graduation now!","info":"Visit kahunaflowers.com, choose Joaquin Miller M.S. as your school, and buy away!"},
 	{"img":"https://s-media-cache-ak0.pinimg.com/236x/51/e0/e9/51e0e9f1269d3ba62c3b6b56dbebc6ec.jpg","title":"Is your dream to become a world-famous clothes designer?","info":"Either way, compete in the Newspaper Fashion Show and show off your fashion skills!"},
 	{"img":"http://www.banthebottle.net/wp-content/uploads/2012/05/RecycleSymbol_wPlasticBottles.jpg","title":"The Plastic to Pennies Drive is coming soon!","info":"Recycle your empty plastic water bottles in exchange for pennies!"},
