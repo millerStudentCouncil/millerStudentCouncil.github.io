0560e987a0a06efd1b56dc2ecbbd885f0778e01f
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-05-21 11:50:36 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -49,7 +49,7 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://saintjamesschool.net/wp-content/uploads/2015/05/BoxTopColor.jpg","title":"Continue collecting Box Tops!","info":"Turn them in to your Homeroom Representative when the collection date comes near!"},
-	{"img":"http://a.theinspiration.com/wp-content/uploads/url19.jpeg","title":"The Look At the Books Drive is from May 20th until June 3rd!","info":"We are collecting _gently_ used books to help raise money for Nepal earthquake relief."},
+	{"img":"http://a.theinspiration.com/wp-content/uploads/url19.jpeg","title":"The Look At the Books Drive is from May 20th until June 3rd!","info":"We are collecting gently used books to help raise money for Nepal earthquake relief."},
 	{"img":"http://files.hgsitebuilder.com/hostgator705321/image/graduation-leis.jpg","title":"Purchase your flowers for graduation now!","info":"Visit kahunaflowers.com, choose Joaquin Miller M.S. as your school, and buy away!"},
 	{"img":"https://s-media-cache-ak0.pinimg.com/236x/51/e0/e9/51e0e9f1269d3ba62c3b6b56dbebc6ec.jpg","title":"Is your dream to become a world-famous clothes designer?","info":"Either way, compete in the Newspaper Fashion Show and show off your fashion skills!"},
 	{"img":"http://www.banthebottle.net/wp-content/uploads/2012/05/RecycleSymbol_wPlasticBottles.jpg","title":"The Plastic to Pennies Drive is coming soon!","info":"Recycle your empty plastic water bottles in exchange for pennies!"},
