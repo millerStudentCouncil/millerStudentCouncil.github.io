bde9a6e29dfe52c3d20c0751f19dd8bcc1f992a0
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-05-27 08:43:12 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -21,28 +21,10 @@ var announcements = [
 	},
 	{
 		"title": {
-			"link":"#newspaperFashionShow",
-			"text":"Compete with your friends in the Newspaper Fashion Show!"
-		},
-	},
-	{
-		"title": {
-			"link":"tinyurl.com/cbdfeedbackform",
-			"text":"Comic Battle Dance Feedback Form"
-		},
-	},
-	{
-		"title": {
 			"link":"#plasticToPennies",
 			"text":"Learn how to get involved with the Plastic to Pennies Drive!"
 		},
 	},	
-	{
-		"title": {
-			"link": "#oscar",
-			"text": "Oscar night is coming to Miller!"
-		},
-	},
 
 
 ]
@@ -51,10 +33,8 @@ var upcomingEvents = [
 	{"img":"http://saintjamesschool.net/wp-content/uploads/2015/05/BoxTopColor.jpg","title":"Continue collecting Box Tops!","info":"Turn them in to your Homeroom Representative when the collection date comes near!"},
 	{"img":"http://a.theinspiration.com/wp-content/uploads/url19.jpeg","title":"The Look At the Books Drive is from May 20th until June 3rd!","info":"We are collecting gently used books to help raise money for Nepal earthquake relief."},
 	{"img":"http://files.hgsitebuilder.com/hostgator705321/image/graduation-leis.jpg","title":"Purchase your flowers for graduation now!","info":"Visit kahunaflowers.com, choose Joaquin Miller M.S. as your school, and buy away!"},
-	{"img":"https://s-media-cache-ak0.pinimg.com/236x/51/e0/e9/51e0e9f1269d3ba62c3b6b56dbebc6ec.jpg","title":"Is your dream to become a world-famous clothes designer?","info":"Either way, compete in the Newspaper Fashion Show and show off your fashion skills!"},
 	{"img":"http://www.banthebottle.net/wp-content/uploads/2012/05/RecycleSymbol_wPlasticBottles.jpg","title":"The Plastic to Pennies Drive is coming soon!","info":"Recycle your empty plastic water bottles in exchange for pennies!"},
 	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
-	{"img":"http://www.techfleece.com/wp-content/uploads/2015/02/The-Oscars-2015-logo.jpg","title":"Oscar Night is on Tuesday, Wednesday, and Thursday at Lunch!","info":"Bring your friends to the gym and watch your classmates' productions!"},
 ]
 
 var spiritDay = "First Summer Outfit Spirit Day" //Leave empty when there is no spirit day
