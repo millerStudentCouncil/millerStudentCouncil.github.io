40b3accba4216ef79364fe60e6c48c51256161bc
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-09-01 21:49:24 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -35,8 +35,8 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-//	{"img":"http://7-themes.com/data_images/out/67/6999869-sunset-beach-waves.jpg","title":"The Sunset Dance is coming up on the 28th!","info":"From 7:00-9:30 p.m.! Make sure to come SEA all the great decorations and delicious food."},
-	{"img":"http://www.laminaresearchcenter.com/images/comingsoon.png","title":"More coming soon!","info":"Stay Tuned!"},
+	{"img":"https://gm1.ggpht.com/UvP_hPHGRhAYNjomU7AsE9RT-_pWB9zMwGMA52ZBPYrzoXdVNfS056cDoBHFzXQYf6v-YTzCQQJNWYiJzY3XJ63ECYgvRCOouXAV9Ji8Du4yl3ecrYpY4yezGGoRNAvjE9C_m6H1jZXt84rLkenZpM7l-gRgTkp0bnv5Ysnpw4wJGccc8I30Rg7HK_mQ1Z4u_6ec4YfG46xpC478t5lZCWZV0DX0DUF_jd19gVpz3hbAP-j-ib3F8eFDuKGFAcxuwXReSdhvXjzvVpOVk9PRa84pdhkygaC8U-uRhzEhowkMIoVAslqqef7UJ-OyKSyOh2glQWyNTn1tKnBGGiBI6NP1Xe3E2ZDWgFM4GkHADvuqURG2Elt_wWZJiJyCL2HQe6F7o2DbgCjDeDnPXEgp13ufhH3yb5cFwuXxNAB0INCDKrMQZsHTsogdDisQvn-9EfzmreeKKYEXg-T5MvK2Vl3032l8U53aryemrQYOtv4amwmqV8NLwIZy18PX5-dc5vwQBh5CO8Add7ckM5_wFXrAlOX7CZP3rqmibpUB_pTggBgWGSOSTUvb4Ynq0pKWUS6PpsuX4LDopu8pMqdgDwVq0bTkvTCCGgXRDWeJjvoAQrezTrRxfIrI3Nk=w1316-h757-l75-ft","title":"Welcome to the Official Miller Student Council Website!","info":"You'll Find All the Latest Info Here."},
+	{"img":"http://www.laminaresearchcenter.com/images/comingsoon.png","title":"More Coming Soon!","info":"Stay Tuned"},
 //	{"img":"http://saintjamesschool.net/wp-content/uploads/2015/05/BoxTopColor.jpg","title":"Continue collecting Box Tops!","info":"Turn them in to your Homeroom Representative when the collection date comes near!"},
 //	{"img":"http://a.theinspiration.com/wp-content/uploads/url19.jpeg","title":"The Look At the Books Drive is from May 20th until June 3rd!","info":"We are collecting gently used books to help raise money for Nepal earthquake relief."},
 //	{"img":"http://files.hgsitebuilder.com/hostgator705321/image/graduation-leis.jpg","title":"Purchase your flowers for graduation now!","info":"Visit kahunaflowers.com, choose Joaquin Miller M.S. as your school, and buy away!"},
