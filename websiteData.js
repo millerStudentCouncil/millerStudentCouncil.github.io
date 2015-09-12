ce3ba5587bf95fe90cc08cbc69501d0427639b73
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-09-12 16:24:43 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -57,6 +57,6 @@ var upcomingEvents = [
 //	{"img":"http://heidicohen.com/wp-content/uploads/social-media-icons-300x225.png","title":"Do you have a Snapchat, Instagram, Twitter, Facebook, or Google+ account?","info":"Follow Miller Student Council to receive updates on Leadership events and activities."},
 ]
 
-//var spiritDay = "Spirit Days (Only For Homeroom Representatives)" Leave empty when there is no spirit day
+var spiritDay = "Spirit Days (Only For Homeroom Representatives)" //Leave empty when there is no spirit day
 
 var formHashes = ["techRequestForm", "dodgeball", "fancyDanceCooking","oscarForm", "feedbackForm", "spiritDayForm", "socialForm", "movieNightForm"] // Hashes to be formatted as google forms, DO NOT INCLUDE spiritDay
