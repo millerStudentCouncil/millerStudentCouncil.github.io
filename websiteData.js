3a1d3196b34b433a31625dc96f313fff7976de89
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-10-06 18:03:39 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -59,8 +59,6 @@ var announcements = [
 ]
 
 var upcomingEvents = [
-	<A HREF="http://millersc.org/#main"><IMG BORDER="0" SRC="http://millersc.org/Drawing.png"></A>
-	
 	{"img":"http://millersc.org/Drawing.png","title":"Welcome to the Official Miller Student Council Website!","info":"Check Back Frequently For All The Latest Information"},
 	{"img":"http://saintjamesschool.net/wp-content/uploads/2015/05/BoxTopColor.jpg","title":"Start collecting Box Tops!","info":"Turn them in to your Homeroom Representative when the collection date (10/14) comes near!"},
 	{"img":"http://pkw.wrdsb.ca/files/2013/10/We-Scare-Hunger.jpg", "title": "Collect food to scare hunger!", "info":"Turn in the cans on collection dates, and don't forget the guidelines!"},
