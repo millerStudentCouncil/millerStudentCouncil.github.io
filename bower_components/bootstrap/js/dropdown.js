9da8c037c95a7363eb416dd009a6abb841fb324d
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2015-10-11 00:24:11 -0700

    Update dropdown.js

--- bower_components/bootstrap/js/dropdown.js
+++ bower_components/bootstrap/js/dropdown.js
@@ -141,6 +141,7 @@
   $(document)
     .on('click.bs.dropdown.data-api', clearMenus)
     .on('click.bs.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
+    .on('touchstart.dropdown.data-api', '.dropdown', function (e) { e.stopPropagation() })
     .on('click.bs.dropdown.data-api', toggle, Dropdown.prototype.toggle)
     .on('keydown.bs.dropdown.data-api', toggle + ', [role=menu], [role=listbox]', Dropdown.prototype.keydown)
 
