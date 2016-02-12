e8ef0c0e972c4c5fa534664f9b7b325bd7669eac
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2016-02-11 19:16:14 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -20,15 +20,15 @@ var announcements = [
 		},
 	},
 	{
-			"title": {
-			"link":"https://docs.google.com/forms/d/17DGAfdU7nO0YMT75ncQ6XFqkJ4Zo1tgG5Ag4OQVw9EA/viewform",
-			"text":"Tree Maintenance Survey"
+		"title": {
+			"link":"#idol",
+			"text":"Miller Idol Rules and Information"
 		},
 	},
 	{
 		"title": {
-			"link":"#idol",
-			"text":"Miller Idol Rules and Information"
+			"link":"#weAreSILENT",
+			"text":"We Are Silent Info"
 		},
 	},
 
@@ -56,6 +56,7 @@ var announcements = [
 
 var upcomingEvents = [
 	{"img":"http://mommybusiness.net/wp-content/uploads/2015/01/Microphone-American-Idol-post.jpg","title":"Miller Idol is coming!","info":" "},
+		{"img":"https://i.ytimg.com/vi/ONZsTBU1Pro/maxresdefault.jpg","title":"We Are Silent","info":"We Are Silent is coming up on April 21! Go silent to raise awareness for children whose voices are silenced. Homerooms with most hours silent, most number of students silent, and most amount of money raised will win prizes. Remember, staying off of social media will gain you extra points!"},
 	{"img":"http://millersc.org/Drawing.png","title":"Welcome to the Official Miller Student Council Website!","info":"Check Back Frequently For All The Latest Information"},
 //	{"img":"http://pkw.wrdsb.ca/files/2013/10/We-Scare-Hunger.jpg", "title": "Collect food to scare hunger!", "info":"Turn in the cans on collection dates, and don't forget the guidelines!"},
 	{"img":"http://www.laminaresearchcenter.com/images/comingsoon.png","title":"More Coming Soon!","info":"Stay Tuned"},
