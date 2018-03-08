62c3b94d21e70de110cd544b0be34f5713080f49
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2018-03-08 12:09:04 -0800

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -132,6 +132,7 @@ var announcements = [
 ]
 
 var upcomingEvents = [	
+	{"img":"http://lisaraffoul.ca/wp-content/uploads/2017/04/leadership_lk-Image-2.jpg","title":"2018-19 Leadership Applications","info":"More information will be provided in the '2018-19 Leadership Applications' tab, located within the 'Events' tab."},
 	{"img":"http://millersc.org/Drawing.png","title":"Welcome to the Official Miller Student Council Website!","info":"Check Back Frequently For All The Latest Information"},
 	{"img":"https://partycity2.scene7.com/is/image/PartyCity/_ml_p2p_pc_badge_taller15?$_ml_p2p_pc_thumb_taller15$&$product=PartyCity/592385_full","title":"Fancy Dance","info":"If you're unsure about what to wear to the Fancy Dance, check out the 'Events' tab and click on Fancy Dance!"},
 	//	{"img":"http://millersc.org/wackyTacky.png","title":"Wacky Tacky Spirit Week","info":"Remember to update the Mismatch day form for today!"},
