8220d10eb60f6be418460686ba7a070906d1b6fb
millerStudentCouncil <millerStudentCouncil@users.noreply.github.com>
2018-05-05 19:24:04 -0700

    Update websiteData.js

--- websiteData.js
+++ websiteData.js
@@ -1,7 +1,7 @@
 var announcements = [
 	
 	
-{
+        {
 		"title": {
 			"link":"#feedbackForm",
 			"text":"Give Feedback to Help Improve this Website!"
@@ -11,9 +11,10 @@ var announcements = [
 	{
 		"title": {
 			"link":"#calendar",
-			"text":"There is a Homeroom Representative Meeting this Wednesday, 4/25/18."
+			"text":"There is a Homeroom Representative Meeting this Wednesday, 5/9/18."
 		},
 	},
+	
 	/*{
 		"title":{
 			"link":"#wackyTackySpiritWeek",
@@ -118,7 +119,7 @@ var announcements = [
 ]
 
 var upcomingEvents = [	
-	{"img":"http://millersc.org/Drawing.png","title":"Welcome to the Official Miller Student Council Website!","info":"Check Back Frequently For All The Latest Information"},
+	{"img":"http://millersc.org/transition.png","title":"We're transitioning to a new website!","info":"Stay tuned to check out our new website soon!"},
 	//{"img":"https://partycity2.scene7.com/is/image/PartyCity/_ml_p2p_pc_badge_taller15?$_ml_p2p_pc_thumb_taller15$&$product=PartyCity/592385_full","title":"Fancy Dance","info":"If you're unsure about what to wear to the Fancy Dance, check out the 'Events' tab and click on Fancy Dance!"},
 	//	{"img":"http://millersc.org/wackyTacky.png","title":"Wacky Tacky Spirit Week","info":"Remember to update the Mismatch day form for today!"},
 //	{"img":"http://www.camdencc.edu/oit/images/Screen-Shot-2016-04-19-at-12-13-16-PM.png","title":"Contact Us","info":"If you have any questions, check our contact us page. Our emails have changed, so please look at that too."},
@@ -128,7 +129,7 @@ var upcomingEvents = [
 //	{"img":"http://www.walkbiketoschool.org/wp-content/themes/WBTS_Custom/images/Logo_WBTS.png","title":"Walk or Bike to School Day","info":"Walk or Bike to School Day is on Wednesday, October 11th. For more information, click on the Events tab and click 'Walk or Bike to School Day.'"}, 
 //	{"img":"https://i.pinimg.com/originals/1f/68/b8/1f68b8001e6843bbe4db3dcc4d540205.jpg","title":"Rainbow Spirit Week","info":"Show your spirit in the Rainbow Spirit Week starting October 9th. To learn more about the spirit week, click on the Events tab and click 'Rainbow Spirit Week.'"},	
 //      {"img":"http://www.carnivaldepot.com/images/ringtoss.jpg", "title":"Come to the Back to School Bash on Thursday, September 14!", "info":"Why did the chalkboard get up in class? To get chalk-olate! There will be various refreshments for sale in the Back to School Bash from 5 to 7PM! Attendance is free and there will be fun carnival games!"},
-        {"img":"http://www.workboard.com/master-team-meetings/images/meeting-peoples.png", "title":"Don't forget to come to the full student council meeting!", "info":"This meeting is on Wednesday, April 25th at 8:20 AM. Check the calendar for more updates and such."},	
+        {"img":"http://www.workboard.com/master-team-meetings/images/meeting-peoples.png", "title":"Don't forget to come to the full student council meeting!", "info":"This meeting is on Wednesday, May 9th at 8:20 AM. Check the calendar for more updates and such."},	
 
 	//	{"img":"http://www.laminaresearchcenter.com/images/comingsoon.png","title":"More Coming Soon!","info":"Stay Tuned"},
 //	{"img":"http://saintjamesschool.net/wp-content/uploads/2015/05/BoxTopColor.jpg","title":"Start collecting Box Tops!","info":"Turn them in to your Homeroom Representative when the collection date (10/14) comes near!"},
